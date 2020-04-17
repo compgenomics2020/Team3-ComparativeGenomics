9185d5ba4d6fd1dfcfcbdffb9e7fccf638ea8a1b
Ulukaya, Gulay Bengu <gulukaya3@gatech.edu>
2020-04-17 18:48:47 -0400

    Update tool_functions.py

--- tool_functions.py
+++ tool_functions.py
@@ -16,14 +16,36 @@ def FastANI():
 
 
 def StringMLST():
-	
+	os.system("mkdir -p stringMLST_analysis; cd stringMLST_analysis")
+	# Download the Listeria MLST scheme
+	os.system("stringMLST.py --getMLST -P listeria/lsa --species Listeria")
+	# Run the tool
+	os.system("stringMLST.py --predict -d /home/projects/group-c/data -P listeria/lsa -k 35 -o StringMLST_Listeria")
 	return
 
 
 
 
 def ChewBBACA():
+	os.system("mkdir -p ChewBBACA_MLST_analysis; cd ChewBBACA_MLST_analysis")
+	# wgMLST schema creation
+	os.system("chewBBACA.py CreateSchema -i /home/projects/group-c/Team3-ComparativeGenomics/functional_annotation_data/comparative_fna -o schema_seed --cpu 6 --ptf Listeria_monocytogenes.trn")
+	# Allele calling with the created wgMLST schema
+	os.system("chewBBACA.py AlleleCall -i /home/projects/group-c/Team3-ComparativeGenomics/functional_annotation_data/comparative_fna -g schema_seed/ -o results_cg --cpu 6 --ptf Listeria_monocytogenes.trn")
+	# Paralog detection
+	os.system("chewBBACA.py RemoveGenes -i results_alleles.tsv -g RepeatedLoci.txt -o alleleCallMatrix_cg")
+	# Test genome quality for cgMLST
+	os.system("chewBBACA.py TestGenomeQuality -i alleleCallMatrix_cg.tsv -n 13 -t 200 -s 5")
+	# Allele calling with the created cgMLST schema
+	os.system("chewBBACA.py AlleleCall -i /home/projects/group-c/Team3-ComparativeGenomics/functional_annotation_data/comparative_fna -g listgenes_core.txt -o results --cpu 6 --ptf Listeria_monocytogenes.trn")
+	# Extract only the loci present in 95% of the matrix
+	os.system("chewBBACA.py ExtractCgMLST -i alleleCallMatrix_cg.tsv -o cgMLST_completegenomes -p 0.95")
+	# Assess the cgMLST allele quality attribution for all the genomes
+	os.system("chewBBACA.py TestGenomeQuality -i cgMLST_all.tsv -n 13 -t 300 -s 5")
 	
+	# For Minimum Spanning Tree, analysis_all/cgMLST_25/cgMLST.tsv can be run with Phyloviz after running the following line 
+	# os.system("chewBBACA.py ExtractCgMLST -i cgMLST_all.tsv -o cgMLST_25 -g removedGenomes_25.txt")
+
 	return
 
 
